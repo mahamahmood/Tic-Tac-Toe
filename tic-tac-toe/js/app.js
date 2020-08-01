594bf85f9f02531612db44128d203031f04e55e5
Maha Mahmood <>
2020-08-01 19:45:06 -0400

    creted a boilerplate and linked css, js, and jqyery to index.html

---
+++ tic-tac-toe/js/app.js
@@ -0,0 +1,4 @@
+console.log('app.js loaded for tic tac toe')
+if (typeof $ == 'undefined'){
+    console.log('oops! I still have to link my jQuery properly!');
+  } else {console.log('I did it! I linked jQuery and this js file properly!')};

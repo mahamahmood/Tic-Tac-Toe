26d142f1907773d0429c96bc21bd0cece14c191c
Maha Mahmood <>
2020-08-02 21:02:36 -0400

    edited app.js(completed restartGame, added playerWon and setTimeout to changeStatus)

--- tic-tac-toe/js/app.js
+++ tic-tac-toe/js/app.js
@@ -1,77 +1,91 @@
+// ===== Checking Linked JS and JQuery ===== //
 // console.log('app.js loaded for tic tac toe')
 // if (typeof $ == 'undefined'){
 //     console.log('oops! I still have to link my jQuery properly!');
 //   } else {console.log('I did it! I linked jQuery and this js file properly!')};
 
 
-// Variables
+// ===== Variables ===== //
 let player = 1;
 const $cell = $('.cell');
+const $restart = $('#restart');
+let playerWon = false;
 
-// ===== Function To Start The Game =====
-const startGame = ()=>{
+// ===== Function To Start The Game ===== //
+const startGame = () => {
     alert('Hi There! Welcome to Tic Tac Toe!')
     alert('The game is set as player vs. player. The first player will play with "X" and the seconde player will play with "Y". Once you decide who start first, click OK to get started. Happy Gaming!')
 }
-// ===== Change Status of Cells Function =====
-const changeStatus = (event)=>{
+
+// ===== Change Status of Cells Function ===== //
+const changeStatus = (event) => {
     let $cellSelected = $(event.currentTarget);
-    if ($cellSelected.hasClass('x') || $cellSelected.hasClass('o')){
+    if ($cellSelected.hasClass('x') || $cellSelected.hasClass('o')) {
         alert('Already used! Please select another box.');
-    }else{
-        if(player === 1){
+    } else {
+        if ((player === 1) && (playerWon === false)) {
             $cellSelected.addClass('x').text('x');
             // check if player 1 won
-            if(checkWin('x')){
-                alert('X Won! Congrats!')
-            }else{
+            if (checkWin('x')) {
+                setTimeout(() => { alert('X Won! Congrats!'), 1000 });
+                 playerWon = true;
+            } else {
                 // set player to 2
                 player = 2;
             }
-        }else{
-            $cellSelected.addClass('o').text('o');
-            // check if player 2 won
-            if(checkWin('o')){
-                alert('O won! Congrats!')
-            }else{
-                // set player back to 1
-                player = 1;
+        } else {
+            if ((player === 2) && (playerWon === false)) {
+                $cellSelected.addClass('o').text('o');
+                // check if player 2 won
+                if (checkWin('o')) {
+                    setTimeout(() => { alert('O Won! Congrats!'), 1000 });
+                    playerWon = true;
+                } else {
+                    // set player back to 1
+                    player = 1;
+                }
             }
-            
         }
     }
 }
-// ===== Win Function =====
+
+// ===== Win Function ===== //
 // There are 8 ways to win. If 3 cells on the same row, colum, or diagonal has the same text, then we have a winner!
 
-const checkWin = (text)=>{
+const checkWin = (text) => {
     //check win for each row(3)
-    if($('#0').hasClass(text) && $('#1').hasClass(text) && $('#2').hasClass(text)){
+    if ($('#0').hasClass(text) && $('#1').hasClass(text) && $('#2').hasClass(text)) {
         return true;
-    }else if($('#3').hasClass(text) && $('#4').hasClass(text) && $('#5').hasClass(text)){
+    } else if ($('#3').hasClass(text) && $('#4').hasClass(text) && $('#5').hasClass(text)) {
         return true;
-    }else if($('#6').hasClass(text) && $('#7').hasClass(text) && $('#8').hasClass(text)){
+    } else if ($('#6').hasClass(text) && $('#7').hasClass(text) && $('#8').hasClass(text)) {
         return true;
-    //check win for each colum(3)
-    }else if($('#0').hasClass(text) && $('#3').hasClass(text) && $('#6').hasClass(text)){
+        //check win for each colum(3)
+    } else if ($('#0').hasClass(text) && $('#3').hasClass(text) && $('#6').hasClass(text)) {
         return true;
-    }else if($('#1').hasClass(text) && $('#4').hasClass(text) && $('#7').hasClass(text)){
+    } else if ($('#1').hasClass(text) && $('#4').hasClass(text) && $('#7').hasClass(text)) {
         return true;
-    }else if($('#2').hasClass(text) && $('#5').hasClass(text) && $('#8').hasClass(text)){
+    } else if ($('#2').hasClass(text) && $('#5').hasClass(text) && $('#8').hasClass(text)) {
         return true;
-    //check win for diagonals (2)
-    }else if($('#0').hasClass(text) && $('#4').hasClass(text) && $('#8').hasClass(text)){
+        //check win for diagonals (2)
+    } else if ($('#0').hasClass(text) && $('#4').hasClass(text) && $('#8').hasClass(text)) {
         return true;
-    }else if($('#2').hasClass(text) && $('#4').hasClass(text) && $('#6').hasClass(text)){
+    } else if ($('#2').hasClass(text) && $('#4').hasClass(text) && $('#6').hasClass(text)) {
         return true;
-    }else{
+    } else {
         return false;
     }
-    
+
 }
 
+// ===== Restart Game ===== //
+const restartGame = () => {
+    location.reload();
+}
 
-// Event listener
+// ===== Event listeners ===== //
 $cell.on('click', changeStatus);
+$restart.on('click', restartGame)
 
+// ===== Invoking Start Game Function ===== //
 // startGame();
